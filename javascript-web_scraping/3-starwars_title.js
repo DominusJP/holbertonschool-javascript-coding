3e8d0a246dd088f2d2600319a8111f54dff7fba3
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-04-26 10:49:05 -0300

    Update 3-starwars_title.js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,34 +1,14 @@
 #!/usr/bin/node
+
 const request = require('request');
+const args = process.argv;
+const URL = 'https://swapi-api.hbtn.io/api/films/';
 
-// Check if movie ID is provided as argument
-if (process.argv.length < 3) {
-  console.error('Usage: node get_movie_title.js <movie_id>');
-  process.exit(1);
-}
-
-// Get movie ID from command line arguments
-const movieId = process.argv[2];
-
-// Construct the URL with movie ID
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-
-// Make a GET request to the Star Wars API
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error(`Failed to fetch movie details. Status code: ${response.statusCode}`);
-    return;
-  }
-
-  try {
-    const movieData = JSON.parse(body);
-    console.log(`Title: ${movieData.title}`);
-  } catch (parseError) {
-    console.error('Error parsing JSON:', parseError);
+request.get(URL + `${args[2]}`, '', (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const film = JSON.parse(body);
+    console.log(film.title);
   }
 });
