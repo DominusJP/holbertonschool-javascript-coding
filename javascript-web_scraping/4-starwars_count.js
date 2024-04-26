5e4d639a8d28f2c90de670f0a87c28d464ebaea1
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-04-26 10:49:15 -0300

    Update 4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
+
 const request = require('request');
 const url = process.argv[2];
-const characterId = 18;
 
-request(url, (err, response, body) => {
+request.get(url, '', (err, res, body) => {
   if (err) {
-    console.log(err);
-  } else {
-    const films = JSON.parse(body).results;
-    const filmsWithWedge = films.filter(film =>
-      film.characters.some(characterUrl => characterUrl.endsWith(`/${characterId}/`))
-    );
-    console.log(filmsWithWedge.length);
+    console.error(err);
   }
+
+  const films = JSON.parse(body).results;
+  const amount = films.filter(film => film.characters.includes('https://swapi-api.hbtn.io/api/people/18/'));
+  console.log(amount.length);
 });
