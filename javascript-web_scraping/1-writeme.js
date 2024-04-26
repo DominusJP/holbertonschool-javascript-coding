4ef3ae19772fed24e0f0e9338dc380065d98eba4
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-04-26 10:39:30 -0300

    Update 1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,14 +1,21 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const writeFile = (filePath, string) => {
-  fs.writeFile(filePath, string, 'utf-8', (err) => {
-    if (err) {
-      console.error(err);
-    }
-  });
-};
+// Check if both file path and string to write are provided as arguments
+if (process.argv.length < 4) {
+  console.error('Usage: node write_file.js <file_path> <string_to_write>');
+  process.exit(1);
+}
 
+// Get file path and string to write from command line arguments
 const filePath = process.argv[2];
-const string = process.argv[3];
-writeFile(filePath, string);
+const stringToWrite = process.argv[3];
+
+// Write string to file
+fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(`String has been written to ${filePath}`);
+});
