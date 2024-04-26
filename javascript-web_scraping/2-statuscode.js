4cdb5745668f4a503af7d33c9d4de3944c2fadd3
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-04-26 10:41:36 -0300

    Update 2-statuscode.js

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,20 +1,12 @@
 #!/usr/bin/node
+
 const request = require('request');
-
-// Check if URL is provided as argument
-if (process.argv.length < 3) {
-  console.error('Usage: node get_status.js <URL>');
-  process.exit(1);
-}
-
-// Get URL from command line arguments
 const url = process.argv[2];
 
-// Make a GET request to the URL
-request.get(url, (error, response) => {
-  if (error) {
-    console.error(error);
-    return;
+request(url, (err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code:', res.statusCode);
   }
-  console.log(`code: ${response.statusCode}`);
 });
