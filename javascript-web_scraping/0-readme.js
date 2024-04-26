7bf9a33fa85bc7f4e697b4cb8cec86118dda5bb2
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-04-26 10:36:50 -0300

    Update 0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,19 @@
-#!/usr/bin/node
-
 const fs = require('fs');
+
+// Check if file path is provided as argument
+if (process.argv.length < 3) {
+  console.error('Usage: node read_file.js <file_path>');
+  process.exit(1);
+}
+
+// Get file path from command line arguments
 const filePath = process.argv[2];
 
+// Read file content
 fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
+    console.error(err);
+    return;
   }
+  console.log(data);
 });
