603e5023e711130d3011c6403fbc679b1ccf15aa
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-05-10 16:36:27 -0300

    Update 6-completed_tasks.js

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,25 +1,19 @@
 #!/usr/bin/node
 
 const request = require('request');
-const args = process.argv;
+const url = process.argv[2];
 
-request.get(args[2], '', (err, res, body) => {
-  if (err) {
-    console.error(err);
-  } else {
-    const dict = {};
-    const users = JSON.parse(body);
-
-    for (const user of users) {
-      if (user.completed) {
-        if (dict[user.userId]) {
-          dict[user.userId] += 1;
-        } else {
-          dict[user.userId] = 1;
-        }
-      }
-    }
-
-    console.log(dict);
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
   }
+  const tasks = JSON.parse(body);
+  const completedTasks = {};
+  tasks.forEach((task) => {
+    if (task.completed) {
+      completedTasks[task.userId] = (completedTasks[task.userId] || 0) + 1;
+    }
+  });
+  console.log(completedTasks);
+});
 });
