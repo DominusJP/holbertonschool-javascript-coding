2707393cecc2b4f6a9e4c96029957b614ea8efe8
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-05-12 02:38:27 -0300

    Create 1-stdin.js

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+console.log("Welcome to Holberton School, what is your name?");
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (data) => {
+  const input = data.trim();
+
+  if (input === 'exit') {
+    console.log("This important software is now closing");
+    process.exit();
+  } else {
+    console.log(`Your name is: ${input}`);
+    console.log("Welcome to Holberton School, what is your name?");
+  }
+});
