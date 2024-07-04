3873089245dbbdfac8bdb7dcc6139e663ceb3dc4
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-07-04 14:01:38 -0300

    Update 0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+const filePath = process.argv[2];
 
-const file = process.argv[2];
-fs.readFile(file, 'utf-8', function (err, content) {
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
-  } else { console.log(content); }
+  } else {
+    console.log(data);
+  }
 });
